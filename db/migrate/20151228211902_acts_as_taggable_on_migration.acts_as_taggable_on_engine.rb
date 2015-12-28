724b7156d4f94b93ca44461ac400856403468bc3
Bryce Boe <bbzbryce@gmail.com>
2015-12-28 16:00:27 -0600

    Squash acts-as-taggable-on migrations.
    Also explicitly set NOT NULL on columns where appropriate.

--- db/migrate/20151228211902_acts_as_taggable_on_migration.acts_as_taggable_on_engine.rb
+++ db/migrate/20151228211902_acts_as_taggable_on_migration.acts_as_taggable_on_engine.rb
@@ -1,31 +1,32 @@
-# This migration comes from acts_as_taggable_on_engine (originally 1)
+# This migration comes from acts_as_taggable_on_engine (bboe combined)
 class ActsAsTaggableOnMigration < ActiveRecord::Migration
-  def self.up
+
+  def change
     create_table :tags do |t|
-      t.string :name
+      t.string :name, null: false
+      t.integer :taggings_count, default: 0, null: false
     end
 
     create_table :taggings do |t|
-      t.references :tag
+      t.references :tag, null: false
 
       # You should make sure that the column created is
       # long enough to store the required class names.
-      t.references :taggable, polymorphic: true
+      t.references :taggable, polymorphic: true, null: false
       t.references :tagger, polymorphic: true
 
       # Limit is created to prevent MySQL error on index
       # length for MyISAM table type: http://bit.ly/vgW2Ql
-      t.string :context, limit: 128
+      t.string :context, limit: 128, null: false
 
-      t.datetime :created_at
+      t.datetime :created_at, null: false
     end
 
-    add_index :taggings, :tag_id
+    add_index :tags, :name, unique: true
+    add_index :taggings,
+              [:tag_id, :taggable_id, :taggable_type, :context, :tagger_id, :tagger_type],
+              unique: true, name: 'taggings_idx'
     add_index :taggings, [:taggable_id, :taggable_type, :context]
   end
 
-  def self.down
-    drop_table :taggings
-    drop_table :tags
-  end
 end
