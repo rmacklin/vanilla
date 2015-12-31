5d008e20d4d542908bdfea8da618ee9088730944
Bryce Boe <bbzbryce@gmail.com>
2015-12-31 10:10:21 -0800

    Add tags_controller_test.
    The create_image function is used to dynamically create an image and associated
    tags for testing purposes. Fixtures cannot easily be used as both 'tags', and
    'taggings' must be created.

---
+++ test/controllers/tags_controller_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class TagsControllerTest < ActionController::TestCase
+  test 'should get index' do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:tags)
+  end
+
+  test 'should not show invalid tag' do
+    assert_raises ActiveRecord::RecordNotFound do
+      get :show, name: 'foo'
+    end
+  end
+
+  test 'should show tag' do
+    create_image('http://foo.com', 'tag1, tag2')
+
+    get :show, name: 'tag1'
+    assert_response :success
+  end
+
+  private
+    def create_image(url, tag_string)
+      image = Image.create(url: url, tag_list: tag_string)
+    end
+end
