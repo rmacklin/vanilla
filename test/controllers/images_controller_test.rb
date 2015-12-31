e1d07952e9bc228f946689ae21ee430db4f58d4f
Bryce Boe <bbzbryce@gmail.com>
2015-12-31 09:52:48 -0800

    Add 'should not' image controller tests when the image isn't found.

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -5,18 +5,18 @@ class ImagesControllerTest < ActionController::TestCase
     @image = images(:appfolio)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:images)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create image" do
+  test 'should create image' do
     assert_difference('Image.count') do
       post :create, image: { url: @image.url }
     end
@@ -24,22 +24,46 @@ class ImagesControllerTest < ActionController::TestCase
     assert_redirected_to image_path(assigns(:image))
   end
 
-  test "should show image" do
+  test 'should not show invalid image' do
+    assert_raises ActiveRecord::RecordNotFound do
+      get :show, id: -1
+    end
+  end
+
+  test 'should show image' do
     get :show, id: @image
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should not edit invalid image' do
+    assert_raises ActiveRecord::RecordNotFound do
+      get :edit, id: -1
+    end
+  end
+
+  test 'should get edit' do
     get :edit, id: @image
     assert_response :success
   end
 
-  test "should update image" do
+  test 'should not update invalid image' do
+    assert_raises ActiveRecord::RecordNotFound do
+      patch :update, id: -1, image: { url: @image.url }
+    end
+  end
+
+  test 'should update image' do
     patch :update, id: @image, image: { url: @image.url }
     assert_redirected_to image_path(assigns(:image))
   end
 
-  test "should destroy image" do
+  test 'should not destroy invalid image' do
+    assert_raises ActiveRecord::RecordNotFound do
+      delete :destroy, id: -1
+    end
+  end
+
+  test 'should destroy image' do
     assert_difference('Image.count', -1) do
       delete :destroy, id: @image
     end
