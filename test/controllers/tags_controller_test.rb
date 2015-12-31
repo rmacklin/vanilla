29ce3c8719fd4a447a5a0a7950071b1af57cc8af
Bryce Boe <bbzbryce@gmail.com>
2015-12-31 15:11:06 -0800

    Use assert_select rather than assigns in tags_controller_test.

--- test/controllers/tags_controller_test.rb
+++ test/controllers/tags_controller_test.rb
@@ -1,10 +1,16 @@
 require 'test_helper'
 
 class TagsControllerTest < ActionController::TestCase
+  setup do
+    @images = []
+    @images << create_image('http://foo.com', 'tag1, tag2')
+    @images << create_image('http://bar.com', 'tag2')
+  end
+
   test 'should get index' do
     get :index
     assert_response :success
-    assert_not_nil assigns(:tags)
+    assert_select '.tag_list li', @images.size
   end
 
   test 'should not show invalid tag' do
@@ -14,15 +20,14 @@ class TagsControllerTest < ActionController::TestCase
   end
 
   test 'should show tag' do
-    create_image('http://foo.com', 'tag1, tag2')
-
     get :show, name: 'tag1'
     assert_response :success
+    assert_select '.card', 1
   end
 
   private
 
   def create_image(url, tag_string)
-    Image.create(url: url, tag_list: tag_string)
+    Image.create!(url: url, tag_list: tag_string)
   end
 end
