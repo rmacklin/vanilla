0669709d562bba42b66a58121cfc4ec775df5996
Bryce Boe <bbzbryce@gmail.com>
2015-12-31 09:51:44 -0800

    Add tag index and show routes, controller, and views.

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,22 @@
+class TagsController < ApplicationController
+
+  before_action :set_tag, only: [:show]
+
+  # GET /tags
+  # GET /tags.json
+  def index
+    @tags = ActsAsTaggableOn::Tag.order(:name).all
+  end
+
+  # GET /images/1
+  # GET /images/1.json
+  def show
+    @images = Image.tagged_with @tag
+  end
+
+  private
+
+    def set_tag
+      @tag = ActsAsTaggableOn::Tag.find_by!(name: params[:name])
+    end
+end
