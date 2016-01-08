68bb8090324122b9632b57ddb7ef535dd05a3c62
Bryce Boe <bbzbryce@gmail.com>
2016-01-08 11:03:51 -0800

    WIP Add/remove multiple tags as a select box.

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,2 +1,6 @@
 module ImagesHelper
+  def image_tags_select(form, image)
+    tags = ActsAsTaggableOn::Tag.order(:name).map { |tag| [tag.name, tag.name] }
+    form.select(:tag_list, tags, {}, class: 'js-select2 form-control', multiple: true)
+  end
 end
