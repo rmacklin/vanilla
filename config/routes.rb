039067cd97c70bdebec7f17af82ef125858d1406
Richard Macklin <richard.github@nrm.com>
2016-01-14 16:27:40 -0800

    Add route for share image action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :images
+  resources :images do
+    member do
+      post :share
+    end
+  end
+
   resources :tags, only: [:index, :show], param: :name
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
