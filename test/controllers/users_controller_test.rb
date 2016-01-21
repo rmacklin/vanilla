4f399d89e813ab207a887516575267ac48e7030d
Richard Macklin <richard.github@nrm.com>
2016-01-21 10:34:59 -0800

    squash implement sign up form

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,8 @@ class UsersControllerTest < ActionController::TestCase
   test 'should get new' do
     get :new
     assert_response :success
+
+    assert_select 'form#new_user'
   end
 
   test 'should create a new user' do
